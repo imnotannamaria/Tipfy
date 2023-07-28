4643d6632fe312c3f084045c668496f9c2ae9856
Anna Maria <aannamariabr@outlook.com>
2023-07-28 17:29:31 -0300

    Music card

---
+++ src/components/MusicCard.tsx
@@ -0,0 +1,48 @@
+import { AspectRatio } from './ui/aspect-ratio'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import Image from 'next/image'
+import { Button } from './ui/button'
+
+interface MusicCardProps {
+  trackName: string
+  artistName: string
+  trackCover: string
+}
+
+export function MusicCard({
+  trackName,
+  artistName,
+  trackCover,
+}: MusicCardProps) {
+  return (
+    <Card className="w-1/2 h-[500px] flex flex-col justify-between">
+      <CardHeader>
+        <CardTitle>{trackName}</CardTitle>
+        <CardDescription>{artistName}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <div className="w-[250px]">
+          <AspectRatio ratio={10 / 10}>
+            <Image
+              src={trackCover}
+              alt=""
+              className="rounded-md object-cover"
+            />
+          </AspectRatio>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full" variant="secondary">
+          Indicar
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
